36444727c38463f9890c839e24911d6790b318d0
xuananhvu23 <anhvxhe161740@fpt.edu.vn>
2025-05-25 20:19:17 +0700

    Third commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Home from "./Pages/Home";
+import { createContext, useEffect, useState } from "react";
+import axios from "axios";
+
+const MyContext = createContext();
+function App() {
+  const [countryList, setCountryList] = useState([]);
+  const [selectedCountry, setSelectedCountry] = useState("");
+
+  useEffect(() => {
+    getCountry("https://countriesnow.space/api/v0.1/countries/");
+  }, []);
+  const getCountry = async (url) => {
+    const responsive = await axios.get(url).then((res) => {
+      setCountryList(res.data.data);
+      console.log(res.data.data);
+    });
+  };
+  const values = {
+    countryList,
+    setSelectedCountry,
+    selectedCountry,
+  };
+  return (
+    <BrowserRouter>
+      <MyContext.Provider value={values}>
+        <Header />
+        <Routes>
+          <Route path="/" exact={true} element={<Home />} />
+        </Routes>
+      </MyContext.Provider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
+export { MyContext };
---
+++ src/Pages/Home/index.js
@@ -0,0 +1,135 @@
+import HomeBanner from "../../components/Header/HomeBanner";
+import banner1 from "../../assets/images/banner1.jpg";
+import banner2 from "../../assets/images/banner2.jpg";
+import banner3 from "../../assets/images/banner3.jpg";
+import banner4 from "../../assets/images/banner4.jpg";
+
+import Button from "@mui/material/Button";
+import { IoIosArrowRoundForward } from "react-icons/io";
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper/modules";
+
+import "swiper/css/pagination";
+import Rating from "@mui/material/Rating";
+
+import ProductItem from "../../components/Header/ProductItem";
+import HomeCat from "../../components/HomeCat";
+
+const Home = () => {
+  var productSliderOptions = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    // arrows: true,
+    // autoplay: true,
+  };
+  return (
+    <>
+      <HomeBanner />
+      <HomeCat />
+
+      <section className="homeProducts">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-3">
+              <div className="sticky">
+                <div className="banner">
+                  <img src={banner1} className="cursor w-100" />
+                </div>
+
+                <div className="banner mt-4">
+                  <img src={banner2} className="cursor w-100" />
+                </div>
+              </div>
+            </div>
+            <div className="col-md-9 productRow">
+              <div className="d-flex align-items-center">
+                <div className="info w-75">
+                  <h3 className="mb-0 hd">BEST SELLERS</h3>
+                  <p className="text-light1 text-sml mb-0">
+                    Do not miss the current offers until the end of May
+                  </p>
+                </div>
+                <Button className="viewAllBtn ml-auto">
+                  View All
+                  <IoIosArrowRoundForward />
+                </Button>
+              </div>
+
+              {/*  */}
+              <div className="product_row w-100 mt-4">
+                <Swiper
+                  slidesPerView={4}
+                  spaceBetween={0}
+                  pagination={{
+                    clickable: true,
+                  }}
+                  modules={[Navigation]}
+                  className="mySwiper"
+                >
+                  <SwiperSlide>
+                    <ProductItem />
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <ProductItem />
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <ProductItem />
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <ProductItem />
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <ProductItem />
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <ProductItem />
+                  </SwiperSlide>
+                </Swiper>
+              </div>
+
+              {/*  */}
+              <div className="d-flex align-items-center mt-5">
+                <div className="info w-75">
+                  <h3 className="mb-0 hd">NEW PRODUCTS</h3>
+                  <p className="text-light1 text-sml mb-0">
+                    New products with updated stocks
+                  </p>
+                </div>
+                <Button className="viewAllBtn ml-auto">
+                  View All
+                  <IoIosArrowRoundForward />
+                </Button>
+              </div>
+
+              {/*  */}
+              <div className="product_row productRow2 w-100 mt-4 d-flex">
+                <ProductItem />
+                <ProductItem />
+                <ProductItem />
+                <ProductItem />
+                <ProductItem />
+                <ProductItem />
+              </div>
+
+              <div className="d-flex mt-4 mb-5 bannerSec">
+                <div className="banner">
+                  <img src={banner3} className="cursor w-100" />
+                </div>
+                <div className="banner">
+                  <img src={banner4} className="cursor w-100" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+export default Home;
